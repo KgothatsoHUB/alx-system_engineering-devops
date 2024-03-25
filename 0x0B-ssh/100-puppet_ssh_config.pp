7d4fbb7050cd31f9e736b3e2e13e69d1b850bd82
KgothatsoHUB <kgothikhoza@gmail.com>
2024-03-25 12:51:37 -0700

    SSH Update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,6 @@
+# Puppet manifest to configure SSH client settings
+# Configures the /etc/ssh/ssh_config file to connect to a server without typing a password
+
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
